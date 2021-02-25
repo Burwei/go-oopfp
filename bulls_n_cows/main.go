16446531a3fd32e42b90ca1922574531b5232927
Burwei <eric19931119@gmail.com>
2021-02-25 17:36:08 +0800

    Using go module.

--- bulls_n_cows/main.go
+++ bulls_n_cows/main.go
@@ -1,10 +1,10 @@
 package main
 
-import "./liboop"
-// import "./libfp"
+// import "github.com/Burwei/go-oopfp/bulls_n_cows/liboop"
+import "github.com/Burwei/go-oopfp/bulls_n_cows/libfp"
 
 func main(){
-	d := liboop.Dealer{} 
-	d.Run()
-	// libfp.Run()
+	// d := liboop.Dealer{} 
+	// d.Run()
+	libfp.Run()
 }
